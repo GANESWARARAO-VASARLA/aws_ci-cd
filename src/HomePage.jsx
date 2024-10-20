7d798d609f86aeba9a1843810caea7dcd83c33b0
Ganeswararao <ganeswararao@kloudworx.com>
2024-10-20 23:17:50 +0530

    update

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,8 +1,38 @@
-import React from "react";
-import Counter from "./Counter";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 function HomePage() {
-  return <div>Homepage</div>;
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        backgroundColor: "lightblue",
+        overflowY: "auto",
+      }}
+    >
+      <h1>Posts</h1>
+      <ul style={{ margin: 0, padding: 0 }}>
+        {data.map((post) => (
+          <li key={post.id}>{post.title}</li> // Displaying the title of each post
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default HomePage;
